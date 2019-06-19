4f6845f66e0f42694b7394d7b290aa594c62f052
cycweeds <cycweeds@gmail.com>
2019-06-19 22:50:22 +0800

    change UIImage extension

--- CWLKit.podspec
+++ CWLKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CWLKit'
-  s.version          = '0.2'
+  s.version          = '0.2.1'
   s.summary          = 'CWLKit: 项目中常用的方法和类'
   s.swift_version = '4.2'
 
--- CWLKit/Classes/Extension/UIImage+Extension.swift
+++ CWLKit/Classes/Extension/UIImage+Extension.swift
@@ -9,8 +9,9 @@ import Foundation
 
 extension UIImage {
     
-    public static func image(_ color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+    public static func image(_ color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
         color.setFill()
         UIRectFill(rect)
